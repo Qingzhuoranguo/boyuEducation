cc5bb49892d09d83002c89a31197d7be5a64ec0e
Aaron Guo <qingzhuoran.guo@gmail.com>
2020-06-01 18:07:47 -0700

    variables and pointers

---
+++ C/pointers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void addOne ( int* m ){
+	/*
+	"int* m"
+	varibale with
+	1. type: inter pointer
+	2. reference: m
+	3. value: y
+	*/
+	*m = *m + 1;
+	/*
+	equivalent to:
+	*m = *(y) = *(&x) = x
+	
+	x = x + 1
+	*/
+
+}
+
+
+int main (){
+	int x = 3; 
+	/* 
+	"int x"
+	variable with 
+	1.type: integer 
+	2. reference: x 
+	3. value: 3
+	*/
+	int* y = &x;
+	/*
+	"int* y"
+	variable with
+	1. type: integer pointer
+	2. reference: y
+	3. value: &x
+	*/
+	addOne(y);
+
+	printf ("x now is: %d\n",x);
+
+
+	return 0;
+}
