5ddadca74ea7b651fba4b2f7a5918c8f35c3815f
Aaron Guo <qingzhuoran.guo@gmail.com>
2020-06-08 15:27:04 -0700

    added solution to 2.3 practice

--- C/2.3_practice.c
+++ C/2.3_practice.c
@@ -3,11 +3,16 @@
 
 int main () {
 	//Q1 declare an integer array with 10 elements
+	int arr [10];
 	
 	//Q2 write a for loop to put 10 numbers (1-10) into the array 
-
+	for (int i = 1; i < 11; i ++){
+		arr[i-1] = i;
+	}
 	//Q3 use printf() and for loop to print out each element in the array
-	
+	for (int i = 0; i < 10; i ++){
+		printf ("%d\n", arr[i]);
+	}
 
 	return 0;
 }
