582f53e60b39d473a47013e75045a27268a2f987
Aaron <qga6@sfu.ca>
2020-06-12 03:03:42 -0700

    created c++ directory, added c++ lambda demo

--- C++/lambda.cpp
+++ C++/lambda.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
 
 using namespace std;
@@ -7,5 +5,10 @@ int main (){
 	int x [10] = {1,2,3,4,5,6,7,8,9,10};
 	
 	cout << [x](){return *(x+1);}() << endl;
+
+	auto f = [&x](/*arguments*/){  //[]: captures: = for all values(local), & for all addrs, 
+		return x [0];
+	};
+	cout << f() << endl;
 	return 0;
 }
