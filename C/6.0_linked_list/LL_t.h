d046f9b235296275b493756f51e88617bc0e86df
Aaron Guo <qingzhuoran.guo@gmail.com>
2020-08-02 17:27:10 -0700

    added sorting function prototype

--- C/6.0_linked_list/LL_t.h
+++ C/6.0_linked_list/LL_t.h
@@ -15,3 +15,22 @@ int LL_pop_head ( struct node_t **Head, struct node_t **Tail );
 int LL_front (struct node_t *Head);
 _Bool LL_find ( struct node_t *Head, int target );
 int LL_delete (struct node_t **Head, struct node_t **Tail, int target);
+
+// Linked List sorting
+/*
+array sorting:
+swap (a[0], a[100])
+array is continuous, if known a[0], also known a[100], a[100] = a[0+100]
+
+Linked list sorting:
+cannot swap (head, tail)
+each notes is not continuous in memory, so cannot do head + index 
+always need to trace for index
+*/
+
+/*
+find the "smallest", put it at the head
+*/
+void LL_selection_sort(struct node_t **Head, struct node_t **Tail);
+
+void LL_merge_sort(struct node_t **Head, struct node_t **Tail);
