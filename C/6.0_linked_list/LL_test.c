02bfaa8aa9c2e8e1a3b28a9590466a6339944e91
Aaron Guo <qingzhuoran.guo@gmail.com>
2020-08-02 17:28:29 -0700

    added selection sort test

--- C/6.0_linked_list/LL_test.c
+++ C/6.0_linked_list/LL_test.c
@@ -20,30 +20,21 @@ int main () {
 		address = &reference 
 		Head's address = &Head
 	*/
+	LL_append ( &Head, &Tail, 6 );
+	LL_append ( &Head, &Tail, 5 );
+	LL_append ( &Head, &Tail, 4 );
 	LL_append ( &Head, &Tail, 1 );
 	LL_append ( &Head, &Tail, 2 );
-	LL_append ( &Head, &Tail, 3 );
-	LL_append ( &Head, &Tail, 4 );
-	LL_append ( &Head, &Tail, 5 );
-	LL_append ( &Head, &Tail, 6 );
+	LL_append ( &Head, &Tail, 1 );
+	LL_append(&Head, &Tail, 6);
+	LL_append(&Head, &Tail, 5);
+	LL_append(&Head, &Tail, 4);
+	LL_append(&Head, &Tail, 1);
+	LL_append(&Head, &Tail, 2);
+	LL_append(&Head, &Tail, 1);
 
-	// Head: does Head change in this function
-	/*
-	LL_print -> doesn't change Head -> pass by reference
-	*/
 	LL_print ( Head );
-
-	// Head: does it change in this fucntion?
-	LL_pop_head ( &Head, &Tail );
-	LL_print ( Head );
-
-	printf("LL_front: %d\n", LL_front (Head) );
-	LL_print ( Head );
-
-	printf("find 6:%d \n", LL_find (Head, 6));
-
-	LL_delete ( &Head, &Tail, 5 );
-	LL_print ( Head );
-	
+	LL_selection_sort(&Head, &Tail);
+	LL_print(Head);
 	return 0;
 }
