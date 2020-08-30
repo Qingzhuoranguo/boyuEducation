f4acaf3bb31d530a1e9b72df748d81ebc3f84578
Aaron Guo <qingzhuoran.guo@gmail.com>
2020-08-30 16:48:11 -0700

    1. stack the structure 2. stack implementation 3. some facts about c++: cout/ cin, namespace

--- C++/stack.cpp
+++ C++/stack.cpp
@@ -1,38 +1,7 @@
 #include <iostream>
-
+#include <stdio.h>
 
 using namespace std;
-
-
-class Account {
-private:
-    int balance;
-    int *data;
-public:
-    Account(){
-        balance = 0;
-        data = (int *)malloc(sizeof(int) * 10);
-    }
-    
-    Account( int old_balance ) {
-        balance = old_balance;
-        data = (int *)malloc(sizeof(int) * 10);
-    }
-
-    ~Account(){
-        free(data);
-    }
-
-    void depo();
-    void with();
-};
-
-int main () {
-    Account aaron;
-    Account boby (100);
-
-}
-
 
 /*
 Stack object 
@@ -41,5 +10,75 @@ Stack object
 3. property -> how many elements 
 4. print () -> print the stack
 5. properties must be private
-
 */
+
+class stack {
+private:
+    int content [100];
+    int currentIndex;
+    int length; 
+public:
+
+    stack () {
+        for (int i = 0; i < 100; i ++ ){
+            content[i] = 0;
+        }
+        currentIndex = 0;
+        length = 0;
+    }
+
+    void push( int num ) {
+        if (currentIndex == 100 ){
+            std::cout << "Error!\n";
+            return;
+        }
+        content [currentIndex] = num;
+        currentIndex ++;
+        length ++;
+    }
+    int pop () {
+        if ( length == 0 ){
+            std::cout << "Error!\n";
+        }
+        currentIndex = currentIndex -1;
+        length = length - 1;
+        return content[currentIndex];
+    }
+    void print () {
+        std::cout << "[ ";
+        for (int i = 0; i < length; i ++ ){
+            std::cout << content[i] << " ";
+        }
+        std::cout << "]" <<  std::endl;
+    }
+};
+
+int main () {
+    // stack a;
+    // a.push(1);
+    // a.push(2);
+    // a.push(3);
+    // a.push(4);
+    // a.push(5);
+    // a.print();
+    // a.pop();
+    // a.pop();
+
+    // a.print();
+    int x = 3;
+    int *y = &x;
+    float a = 3.0;
+    double b = 14.0;
+    cout << "the number is: "
+         << x << endl
+         << y << endl
+         << a << endl
+         << b << endl
+         << endl;
+    
+    int m = 0;
+    cin >> m;
+    cout << m << endl;
+}
+
+ 
